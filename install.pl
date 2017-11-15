3ecf6eb3714c96c8d1e9bda89fb4ad4d6bf654ea
Jason Zavaglia <jason.zavaglia@gmail.com>
2017-11-15 23:55:06 +1100

    added instructions into install.pl script

--- install.pl
+++ install.pl
@@ -8,6 +8,8 @@ use Cwd;
 my $dotfilesDir = dirname(Cwd::abs_path($0));
 my $targetDir = $ENV{"HOME"};
 
+print "# run the output of this file into a sh if you are happy with the commands, e.g. ./install.pl | sh\n"
+
 while (<$dotfilesDir/.* $dotfilesDir/*>) {
 	my $fileName = basename($_);
 	if ($fileName eq "install.pl" || ! -f $fileName) {
@@ -17,7 +19,7 @@ while (<$dotfilesDir/.* $dotfilesDir/*>) {
 		my $targetFile = File::Spec->catfile($targetDir, $fileName);
 		my $execStr = "ln -s '$sourceFile' '$targetFile'";
 		if (-e $targetFile) {
-			$execStr = "# " . $execStr . " # file exists"
+			$execStr = "# " . $execStr . " # file exists" 
 		}
 		print $execStr . "\n";
 	}
